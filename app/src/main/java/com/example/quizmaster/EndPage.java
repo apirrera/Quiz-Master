1e4c492acea8039091df25e02dd6e3b694765f68
apirrera <pirreraantonella@gmail.com>
2021-01-11 22:13:51 -0500

    Quiz Master App (In progress code)
    Signed-off-by: apirrera <pirreraantonella@gmail.com>

--- app/src/main/java/com/example/quizmaster/EndPage.java
+++ app/src/main/java/com/example/quizmaster/EndPage.java
@@ -3,19 +3,20 @@ package com.example.quizmaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EndPage extends AppCompatActivity {
+    public TextView Results;
+    int Results = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_page);
+
+        Results = findViewById(R.id.Results);
     }
 
-    public void Retake(View view) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-    }
 }
--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -3,19 +3,43 @@ package com.example.quizmaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+        private CheckBox answer1, answer2, incorrect1, incorrect2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void Complete(View view) {
-        Intent i = new Intent(this, EndPage.class);
-        startActivity(i);
-    }
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+        incorrect1 = findViewById(R.id.incorrect1);
+        incorrect2 = findViewById(R.id.incorrect2);
+        Results = findViewById(R.id.activity_end_page.Results);
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        if (answer1.isChecked())
+                                public void Results (View v){
+                                Results = Results + 25;
+                                Results(Results);
+                        }
+                        else
+                }
+
+                }
+        }
+
+        public void Results(int score) {
+                TextView scoreView = (TextView) findViewById(R.id.Results);
+                scoreView.setText(String.valueOf(score));
+        }
+
+        }
 }
