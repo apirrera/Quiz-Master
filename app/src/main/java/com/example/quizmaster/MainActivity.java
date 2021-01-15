8679deaf382ed43780d479d66fdce9ebf9e0aa49
apirrera <pirreraantonella@gmail.com>
2021-01-14 20:27:55 -0500

    Set intent for MainActivity.java to open EndPage.java
    I created the original intent to connect MainActivity with EndPage. I added an id to the button.
    
    Next steps try to pass variables. Observation -  (Retake button takes end user back to activity_main.xml yet did not include any code for that action) Need to specific EndPage.java code.
    
    Signed-off-by: apirrera <pirreraantonella@gmail.com>

--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -3,43 +3,28 @@ package com.example.quizmaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-        private CheckBox answer1, answer2, incorrect1, incorrect2;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        answer1 = findViewById(R.id.answer1);
-        answer2 = findViewById(R.id.answer2);
-        incorrect1 = findViewById(R.id.incorrect1);
-        incorrect2 = findViewById(R.id.incorrect2);
-        Results = findViewById(R.id.activity_end_page.Results);
-
-        answer1.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.Submit);
+        button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                        if (answer1.isChecked())
-                                public void Results (View v){
-                                Results = Results + 25;
-                                Results(Results);
-                        }
-                        else
+                        openEndPage();
                 }
+        });
+}
 
-                }
-        }
-
-        public void Results(int score) {
-                TextView scoreView = (TextView) findViewById(R.id.Results);
-                scoreView.setText(String.valueOf(score));
-        }
-
+        private void openEndPage() {
+                Intent intent = new Intent(this, EndPage.class);
+                startActivity(intent);
         }
 }
