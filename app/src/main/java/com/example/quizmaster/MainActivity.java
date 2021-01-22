0d7bc35e2851c8708049de98f9eeb6120179c57c
apirrera <pirreraantonella@gmail.com>
2021-01-22 01:27:54 -0500

    Set intent for MainActivity.java to open EndPage.java
    I created the original intent to connect MainActivity with EndPage. I added an id to the button.
    
    Next steps try to pass variables. Observation -  (Retake button takes end user back to activity_main.xml yet did not include any code for that action) Need to specific EndPage.java code.
    
    Signed-off-by: apirrera <pirreraantonella@gmail.com>

--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -1,30 +1,50 @@
 package com.example.quizmaster;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //defining values here
+    int Results = 0;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button button = (Button) findViewById(R.id.Submit);
-        button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                        openEndPage();
-                }
-        });
-}
+    //This is the method to change the score and display it
+    public void displayResults(int score) {
+        TextView scoreView = findViewById(R.id.Results);
+        scoreView.setText(String.valueOf(score));
+    }
 
+    // The actual code with variables and outputs Checkbox else/if code
+    public void Complete(View v) {
+        CheckBox Answer1 = (CheckBox) findViewById(R.id.checkbox1a);
+        CheckBox Answer2 = (CheckBox) findViewById(R.id.checkbox1b);
+        CheckBox Incorrect1 = (CheckBox) findViewById(R.id.checkbox1c);
+        CheckBox Incorrect2 = (CheckBox) findViewById(R.id.checkbox1d);
+        if (Answer1.isChecked() && Answer2.isChecked()) {
+            Results = Results + 25;
+            displayResults(Results);
+        }
+        if (Incorrect1.isChecked() && Incorrect2.isChecked()) {
+        }
+
+
+
+
+        /*Intent to send results to 2nd page
         private void openEndPage() {
                 Intent intent = new Intent(this, EndPage.class);
                 startActivity(intent);
-        }
+        } */
+    }
+
+
 }
