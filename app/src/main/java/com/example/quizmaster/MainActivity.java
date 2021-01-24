a63bb45301efe0dd83e0a907df09e6af29fa02ea
apirrera <pirreraantonella@gmail.com>
2021-01-23 23:31:33 -0500

    Created function to hide complete button and reveal submit button.  (Bug is that it stops the grading function)
    I created an onClick button when the Complete button is clicked then the complete button disappears and the retake button appears.
    
    A negative side effect was that it caused the grading function to stop working. I need to seek a solution to have the grading function and transition.
    
    Signed-off-by: apirrera <pirreraantonella@gmail.com>

--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quizmaster;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,28 +26,58 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    // The actual code with variables and outputs Checkbox else/if code
+   /* public void displayCorrect(){
+        TextView scoreView = findViewById(R.id.summary);
+    } */
+
+    // Defines checkbox variables to match with ids and validates checkbox answers and displays incorrect and correct
     public void Complete(View v) {
-        CheckBox Answer1 = (CheckBox) findViewById(R.id.checkbox1a);
-        CheckBox Answer2 = (CheckBox) findViewById(R.id.checkbox1b);
-        CheckBox Incorrect1 = (CheckBox) findViewById(R.id.checkbox1c);
-        CheckBox Incorrect2 = (CheckBox) findViewById(R.id.checkbox1d);
-        if (Answer1.isChecked() && Answer2.isChecked()) {
+        CheckBox Q1Answer1 = (CheckBox) findViewById(R.id.checkbox1a); //Here you see the type variable name, type again linked to the Id
+        CheckBox Q1Answer2 = (CheckBox) findViewById(R.id.checkbox1b);
+        CheckBox Q1Incorrect1 = (CheckBox) findViewById(R.id.checkbox1c);
+        CheckBox Q1Incorrect2 = (CheckBox) findViewById(R.id.checkbox1d);
+        CheckBox Q2Answer1 = (CheckBox) findViewById(R.id.checkbox2a); //Here you see the type variable name, type again linked to the Id
+        CheckBox Q2Answer2 = (CheckBox) findViewById(R.id.checkbox2b);
+        CheckBox Q2Incorrect1 = (CheckBox) findViewById(R.id.checkbox2c);
+        CheckBox Q2Incorrect2 = (CheckBox) findViewById(R.id.checkbox2d);
+
+        if (Q1Answer1.isChecked() && Q1Answer2.isChecked()) { //Here the variables are referenced, it states if these two are checked then add 25 to total score
             Results = Results + 25;
-            displayResults(Results);
+            //displayResults(Results);
         }
-        if (Incorrect1.isChecked() && Incorrect2.isChecked()) {
+        if (Q1Incorrect1.isChecked() && Q1Incorrect2.isChecked()) {
         }
 
+        // Question 2 Checkbox
+        if (Q2Answer1.isChecked() && Q2Answer2.isChecked()) { //Here the variables are referenced, it states if these two are checked then add 25 to total score
+            Results = Results + 25;
+            //displayResults(Results);
+        }
+        if (Q2Incorrect1.isChecked() && Q2Incorrect2.isChecked()) {
+        }
 
+        // Defines edittext variables to match with ids and validates checkbox answers and displays incorrect and correct
+        EditText Question3 = (EditText) findViewById(R.id.entertext);
+        String Question3String = Question3.getText().toString().trim();
 
+        if (Question3String.equalsIgnoreCase("Test")) {
+            Results = Results + 25;
+            // displayResults(Results);
+        }
 
-        /*Intent to send results to 2nd page
-        private void openEndPage() {
-                Intent intent = new Intent(this, EndPage.class);
-                startActivity(intent);
-        } */
+        // Defines radiobutton variables to match with ids and validates checkbox answers and displays incorrect and correct
+        RadioButton Q4ARadiobutton = (RadioButton) findViewById(R.id.Answer4);
+        if (Q4ARadiobutton.isChecked()) {
+            Results = Results + 25;
+            //displayResults(Results);
+        }
     }
 
-
+    public void SubmitClick(View view) {
+        Button SubmitClick = (Button) findViewById(R.id.Submit);
+        Button RetakeClick = (Button) findViewById(R.id.Retake);
+        SubmitClick.setVisibility(View.GONE);
+        RetakeClick.setVisibility(View.VISIBLE);
+        displayResults(Results);
+    }
 }
